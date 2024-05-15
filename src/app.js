c6ff349ab696e4b9a2cd7a44261fbb73b4bc53c3
Rohit Kumar <rohit7091407599@gmail.com>
2024-05-15 13:22:02 +0530

    Change the origin

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const app = express()
 
 // configurations
 app.use(cors({
-    origin: process.env.CORS_ORIGIN ,
-    // origin: ["http://localhost:5173", "http://192.168.201.236:5173"],
+    // origin: process.env.CORS_ORIGIN ,
+    origin: ["http://localhost:5173", "https://videotube-pi.vercel.app"],
     credentials: true,
 }))
 
